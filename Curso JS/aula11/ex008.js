21d5828e09cabab1780635a021d6b324a1a1db3a
Weslly Silva <wesllyinfo@gmail.com>
2020-04-18 11:56:30 -0300

    Sistema funcionado corretamente

--- Curso JS/aula11/ex008.js
+++ Curso JS/aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('Ola mundo')
+var vel = 71
+console.log(`A velocidade do carro e de ${vel}km/h`)
+if (vel > 70) { // condição simples!
+    console.log('Você ultrapassou a velociade permitida. MULTADO!')
+}
+console.log('Dirija sempre utilizando o cinto de segurança!')
